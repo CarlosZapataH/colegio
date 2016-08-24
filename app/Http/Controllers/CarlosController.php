aa4f19ae150473b24751dd8926934d345056f2e4
Carlos Zapata <czapatahancco@gmail.com>
2016-08-23 22:08:52 -0500

    iniciando el proyecto

---
+++ app/Http/Controllers/CarlosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CarlosController extends Controller
+{
+    //
+    public function Carlos(){
+    	return"Hello";
+    }
+}
